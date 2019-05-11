aa727e4e7bf8ce981bb0a2b1427ad82a0b1ab723
Heidi Hlavinka <hchlav@gmail.com>
2019-05-11 23:11:04 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190511212214) do
+ActiveRecord::Schema.define(version: 20180314210852) do
 
   create_table "user", force: :cascade do |t|
-    t.string "username"
-    t.string "password_digest"
-  end
-
-  create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password_digest"
   end
